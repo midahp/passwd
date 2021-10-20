da7111f9bd54b4fa4041e64bf8d9505a1861104b
Rafael te Boekhorst <boekhorst@b1-systems.de>
2021-10-20 11:01:04 +0000

    latest ChangePassword.php

--- src/Handler/Api/ChangePassword.php
+++ src/Handler/Api/ChangePassword.php
@@ -10,14 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use \Passwd_Driver as Driver;
 use \Horde\Core\Config\State as Configuration;
 use \Horde_Registry;
-
-
-
-
-
-
-// this is for testing and should be removed
-use \Horde_Session;
+use \Horde_Auth;
 
 
 /**
@@ -29,23 +22,20 @@ class ChangePassword implements RequestHandlerInterface
     protected ResponseFactoryInterface $responseFactory;
     protected StreamFactoryInterface $streamFactory;
     private Driver $driver;
-    private Configuration $config;
+    public Configuration $config;
     private Horde_Registry $registry;
-
-
-    // this is for testing and should be removed
-    protected Horde_Session $session;
+    public $reason;
+    public $status;
     
 
 
    
     public function __construct(
-        Horde_Session $session, // this is for testing and should be removed
         ResponseFactoryInterface $responseFactory,
         StreamFactoryInterface $streamFactory,
         Driver $driver,
         Configuration $config,
-        Horde_Registry $registry
+        Horde_Registry $registry 
     )
     {
         $this->responseFactory = $responseFactory;
@@ -53,8 +43,6 @@ class ChangePassword implements RequestHandlerInterface
         $this->driver = $driver;
         $this->config = $config;
         $this->registry = $registry;
-        // below is for testing and should be removed
-        $this->session = $session;
     }
 
     /**
@@ -63,60 +51,63 @@ class ChangePassword implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         
-        // fertig? mach unittests für den Controller (probleme mit Globals? Mach Mock vom Inhalt von Globals)
-        
-        
-        // testing request object
-        $testObjectFromRequest = [
-            "username" => "string",
-            "oldPassword" => "string",
-            "newPassword" => "string",
-            "newPasswordConfirm" => "string"
-        ];
-        
-
-        $testObjectFromRequest = json_encode($testObjectFromRequest);
-
-        $token = (string) $this->session->getToken();
-
-
+            
+        /**
+         * For testing the code, uncommented code below and comment the line "$post = $request->getParsedBody();"
+         */
+        //    // testing request object
+        //    $testObjectFromRequest = [
+        //     "username" => "charles",
+        //     "oldPassword" => "test123",
+        //     "newPassword" => "test123",
+        //     "newPasswordConfirm" => "test123"
+        // ];
+        // $testObjectFromRequest = json_encode($testObjectFromRequest);
+        // $post = json_decode($testObjectFromRequest);
         
         $post = $request->getParsedBody();
-        $user = $post['username'];
-        $currentPassword = $post['oldPassword'];
-        $newPassword = $post['newPassword'];
-        $confirmPassword = $post['newPasswordConfirm'];
+        $user = $post->username;
+        $currentPassword = $post->oldPassword;
+        $newPassword = $post->newPassword;
+        $confirmPassword = $post->newPasswordConfirm;
 
         $jsonData = ['success' => false, 'message' => ''];
 
-
-        try {
-            $this->verifyPassword($user, $confirmPassword, $currentPassword, $newPassword);
-            $this->driver->changePassword($user, $currentPassword, $newPassword);
-            $jsonData['success'] = true;
-        } catch (\Throwable $th) {
-            //throw $th;
-            $jsonData['message'] = $th->getMessage();
+        if ($this->verifyPassword($user, $confirmPassword, $currentPassword, $newPassword)) {
+            
+            try {
+                $this->driver->changePassword($user, $currentPassword, $newPassword);
+                $jsonData['success'] = true;
+                // $jsonData['statuscode'] = $this->status;
+            } catch (\Throwable $th) {
+                $jsonData['message'] = $th->getMessage();
+                $jsonData['success'] = false;
+                // $jsonData['statuscode'] = 404;
+            }
         }
+         else  {
+            $jsonData['message'] = $this->reason;
+            $jsonData['success'] = false;
+            
+        }
+
+        
 
         $jsonString = json_encode($jsonData);
 
-        /**
-        * This is the code that I want to use for the real app. Currently it is commented, because Im testing with the code blow
-        */ 
-        // $body = $this->streamFactory->createStream($jsonString);
-        // return $this->responseFactory->createResponse(200)->withBody($body)->withHeader('Content-Type', 'application/json');
-    
-        $userid = $this->registry->getAuth();
-        $userid = $this->registry->getAuthInfo();
-        $conf = $this->config;
+        
+        // sending the response object
+        $body = $this->streamFactory->createStream($jsonString);
+        $response = $this->responseFactory->createResponse($this->status, $this->reason)->withBody($body)
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus($this->status, $this->reason);
 
-        // testing request object
-        $body = $this->streamFactory->createStream(print_r($userid));
-        return $this->responseFactory->createResponse(200)->withBody($body)->withHeader('Horde-Session-Token', $token)->withAddedHeader('Content-Type', 'application/json');
+        // For debuggin use: \Horde::debug($response, '/dev/shm/test2', false);
+        return $response;
+
         
     }
-
+    
     /**
      * @param string $backend_key  Backend key.
      */
@@ -125,28 +116,96 @@ class ChangePassword implements RequestHandlerInterface
         
         // Implementiere Checks von basic.php: Extra Notizen mit was noch angepasst werden muss (TODO) (auf English)
           
-        $notification = $this->notification;
-        $conf = $this->config;
+        
+        $conf = $this->config->toArray();
         $registry = $this->registry;
         $userid = $registry->getAuth();
-        $userPassword = $registry->getAuthCredential();
-        
-        
+        $credentials = $registry->getAuthCredential();
+        $userPassword = (string) $credentials['password'];
 
-        // THIS SHOULD BE DONE BY MIDDLEWARE?
-
+        // output if all below checks pass
+        $output = true;
+        $this->reason = "";
+        $this->status = 200;
+        
+       
+        // check if the username is the correct username... users can only change their own passwords right?
+        if ($userid !== $user){
+            $this->reason = "You can't change password for user ".$user.". Please enter your own correct username.";
+            $this->status = (int) 403;
+            $output = false;
+            return;
+        }
+        
         // Check for users that cannot change their passwords.
         if (in_array($userid, $conf['user']['refused'])) {
-            $notification->push(sprintf(_("You can't change password for user %s"), $userid), 'horde.error');
-            return false;
+            $this->reason = "You do dont have permission to change password as user ".$user."";
+            $this->status = (int) 403;
+            $output = false;
+            return;
+        }   
+        
+        // Check that oldpassword is current password
+        if ($currentPassword !== $userPassword) {
+            $this->reason = "Please enter your current password correctly";
+            $this->status = (int) 404;
+            $output = false;
+            return;
         }
 
-        // other checks are in basic.php, will try to take over as many as possible
+        // check that new password is different from old password
+        if ($currentPassword == $newPassword) {
+            $this->reason = "Please enter a different password";
+            $this->status = (int) 404;
+            $output = false;
+            return;
+        }
 
-        return;
+        // Check that the new password is typed correctly
+        if ($newPassword !== $confirmPassword){
+            $this->reason = "Please make sure you enter your new password correctly";
+            $this->status = (int) 404;
+            $output = false;
+            return;
+        }
 
-       
+        // OTHER TESTS FROM BASIC.PHP NOT YET IMPLEMENTED (WILL IMPLEMENT AS MANY AS POSSIBLE AND AS IS USEFULL)
+        // $b_ptr = $this->_backends[$backend_key];
+
+        // try {
+        //     Horde_Auth::checkPasswordPolicy($this->_vars->newpassword0, isset($b_ptr['policy']) ? $b_ptr['policy'] : array());
+        // } catch (Horde_Auth_Exception $e) {
+        //     $notification->push($e, 'horde.warning');
+        //     return;
+        // }
+
+        // // Do some simple strength tests, if enabled in the config file.
+        // if (!empty($conf['password']['strengthtests'])) {
+        //     try {
+        //         Horde_Auth::checkPasswordSimilarity($this->_vars->newpassword0, array($this->_userid, $this->_vars->oldpassword));
+        //     } catch (Horde_Auth_Exception $e) {
+        //         $notification->push($e, 'horde.warning');
+        //         return;
+        //     }
+        // }     $b_ptr = $this->_backends[$backend_key];
+
+        // try {
+        //     Horde_Auth::checkPasswordPolicy($this->_vars->newpassword0, isset($b_ptr['policy']) ? $b_ptr['policy'] : array());
+        // } catch (Horde_Auth_Exception $e) {
+        //     $notification->push($e, 'horde.warning');
+        //     return;
+        // }
+
+        // // Do some simple strength tests, if enabled in the config file.
+        // if (!empty($conf['password']['strengthtests'])) {
+        //     try {
+        //         Horde_Auth::checkPasswordSimilarity($this->_vars->newpassword0, array($this->_userid, $this->_vars->oldpassword));
+        //     } catch (Horde_Auth_Exception $e) {
+        //         $notification->push($e, 'horde.warning');
+        //         return;
+        //     }
+        // }
+
+        return $output;
     }
-
-     
 }
